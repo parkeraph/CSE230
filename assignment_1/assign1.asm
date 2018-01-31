bcfce87589b0248079e317274748b925601ab3aa
Andrew Hampton <parker@squaredev.com>
2018-01-31 16:20:13 -0700

    Task 5 and Task 6 completed and tested

--- assignment_1/assign1.asm
+++ assignment_1/assign1.asm
@@ -2,7 +2,7 @@
 name: .asciiz "andrew hampton"
 promt: .asciiz "Enter a number"
 new_line: .asciiz "\n"
-val1: .word 0xFFFFFFFF
+val1: .word 0
 val2: .word 0
 val3: .word 0
 .text
@@ -31,7 +31,14 @@ main:
 		lui $v1 0x1001 #bhase register for mem store
 		sw $s1 0x20($v1) #location of val1 is $v1 + 20
 #5. Store the value in $s2 into the memory labeled “val2”
+	#TASK 5 
+		lui $v1 0x1001 #reset base reg for good mesure
+		sw $s2 0x24($v1) #write to location of 
 #6. Calculate the value of $s0 - $s1 + $s2 and store the result in the memory labeled “val3”
+	#TASK 6
+		add $s3 $s1 $s2 # add s1 and s2 and store in s3
+		sub $s4 $s0 $s3 #subtract s0 from the addition of s1 and s2
+		sw $s4 0x28($v1)#store in val3
 #7. Print the following output each on its own line
 #a. Your name
 #b. The value in val1
